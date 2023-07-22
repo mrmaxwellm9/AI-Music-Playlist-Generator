2cedee168ec5ebf3cc9ae3c6ff03a4aceebcf08b
Max Mayer <mrmaxwellm9@gmail.com>
2023-07-22 09:25:35 -0700

    added alternative ket method

--- AiPlaylistMaker/settings.py
+++ AiPlaylistMaker/settings.py
@@ -23,8 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+
+try:
+    with open('etc/secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
+except:
+    print("Failed to open with file, attempting environment variable")
+else:
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
